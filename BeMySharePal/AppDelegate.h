4670d67c59a5eaa5231f8b86e5ccb9bc3b88ced1
Radu <radumvlad@yahoo.com>
2016-02-02 00:36:32 +0200

    Initial commit.

---
+++ BeMySharePal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeMySharePal
+//
+//  Created by Radu on 01/02/16.
+//  Copyright © 2016 FMI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
